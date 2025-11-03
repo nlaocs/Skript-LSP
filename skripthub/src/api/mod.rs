9edc29bfe41c78c7a5061e29d1d3e6f59cbb9f97
あお <113911070+nlaocs@users.noreply.github.com>
2025-11-03 23:27:41 +0900

    Add fetch_data function to retrieve AbstractAddonSyntaxList from API and implement test for response parsing

--- skripthub/src/api/mod.rs
+++ skripthub/src/api/mod.rs
@@ -1,2 +1,10 @@
 mod internal_utils;
 pub mod types;
+
+use types::AbstractAddonSyntaxList;
+pub fn fetch_data() -> Result<AbstractAddonSyntaxList, reqwest::Error> {
+    const URL: &str = "https://skripthub.net/api/v1/addonsyntaxlist/";
+    // この関数自体実行されるのは最初の一度限りなので、blockingで良い
+    let resp: AbstractAddonSyntaxList = reqwest::blocking::get(URL)?.json()?;
+    Ok(resp)
+}
