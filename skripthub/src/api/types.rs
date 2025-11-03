e645dbc1bdea6627e4ee139d85187d79f081fa78
あお <113911070+nlaocs@users.noreply.github.com>
2025-11-03 20:11:27 +0900

    Add serde and serde-intern dependencies with initial data structures

---
+++ skripthub/src/api/types.rs
@@ -0,0 +1,61 @@
+use serde_intern::intern_arc_str;
+use std::sync::Arc;
+
+#[derive(serde::Deserialize, Debug, Clone)]
+#[serde(rename_all = "lowercase")]
+pub enum SyntaxType {
+    Event,
+    Condition,
+    Effect,
+    Expression,
+    Type,
+    Function,
+    Section,
+    Structure,
+}
+
+#[derive(serde::Deserialize, Debug, Clone)]
+pub struct SupportingPlugin {
+    #[serde(deserialize_with = "intern_arc_str")]
+    pub name: Arc<str>, // required [1..80]
+    #[serde(deserialize_with = "intern_arc_str")]
+    pub link: Arc<str>, // <=200
+}
+
+#[derive(serde::Deserialize, Debug, Clone)]
+pub struct InternalAddon {
+    #[serde(deserialize_with = "intern_arc_str")]
+    pub name: Arc<str>, // required [1..40]
+    #[serde(deserialize_with = "intern_arc_str")]
+    pub link_to_addon: Arc<str>, // <uri> required [1..200]
+    pub usage_score: f64,
+}
+#[derive(serde::Deserialize, Debug, Clone)]
+pub struct AbstractAddonSyntaxListEntry {
+    pub id: i64,
+    pub creator: i64,
+    pub title: String,       // required [1..100]
+    pub description: String, // <=8000
+    #[serde(deserialize_with = "intern_arc_str")]
+    pub syntax_pattern: Arc<str>, // required [1..3500]
+    #[serde(deserialize_with = "intern_arc_str")]
+    pub compatible_addon_version: Arc<str>, // <=200
+    #[serde(deserialize_with = "intern_arc_str")]
+    pub compatible_minecraft_version: Arc<str>, // <=200
+    pub syntax_type: SyntaxType,
+    #[serde(deserialize_with = "intern_arc_str")]
+    pub get_syntax_type_css_class: Arc<str>, // 使うのか...?
+    pub required_plugins: Vec<SupportingPlugin>,
+    pub addon: InternalAddon,
+    pub type_usage: Option<String>,   // <=2000
+    pub return_type: Option<String>,  // <=200
+    pub event_values: Option<String>, // <=1000
+    pub json_id: Option<String>,      // <=200
+    pub event_cancellable: bool,
+    pub created_at: String,       // <date-time>
+    pub updated_at: String,       // <date-time>
+    pub entries: Option<String>,  // <=3500
+    pub keywords: Option<String>, // <=200
+    pub mark_as_removed: bool,
+    pub removed_since: Option<String>, // <=100
+}
