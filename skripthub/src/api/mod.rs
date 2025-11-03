dbdf6f6563b0724ffbba59cdae575f52029272ef
あお <113911070+nlaocs@users.noreply.github.com>
2025-11-03 23:41:12 +0900

    Replace reqwest with ureq for fetch_data function and update dependencies in Cargo.toml

--- skripthub/src/api/mod.rs
+++ skripthub/src/api/mod.rs
@@ -2,9 +2,12 @@ mod internal_utils;
 pub mod types;
 
 use types::AbstractAddonSyntaxList;
-pub fn fetch_data() -> Result<AbstractAddonSyntaxList, reqwest::Error> {
+pub fn fetch_data() -> Result<AbstractAddonSyntaxList, ureq::Error> {
     const URL: &str = "https://skripthub.net/api/v1/addonsyntaxlist/";
     // この関数自体実行されるのは最初の一度限りなので、blockingで良い
-    let resp: AbstractAddonSyntaxList = reqwest::blocking::get(URL)?.json()?;
+    let resp = ureq::get(URL)
+        .call()?
+        .body_mut()
+        .read_json::<AbstractAddonSyntaxList>()?;
     Ok(resp)
 }
